c3f5e2fb46b09f6f615db12c762fb6212e60878d
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-01-19 12:21:21 -0300

    Retirando Delay

--- pages/tempo.js
+++ pages/tempo.js
@@ -6,7 +6,7 @@ function Tempo(props) {
   return (
     <div>
       <div>{dynamicDateString} (dinâmico)</div>
-      <div>{props.staticDateString} (estático - com delay e revalidação branch main)</div>
+      <div>{props.staticDateString} (estático)</div>
     </div>
   )
 }
@@ -14,9 +14,7 @@ function Tempo(props) {
 // getStaticProps só executa no build ou quando bater o tempo de revalidação
 export async function getStaticProps() {
   console.log('> Passando pelo getStaticProps();');
-  console.log('> Adicionando delay de 5 segundos');
 
-  await delay(5000);
   const staticDate = new Date();
   const staticDateString = staticDate.toGMTString();
 
@@ -27,5 +25,4 @@ export async function getStaticProps() {
     revalidate: 1
   }
 }
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 export default Tempo; 
